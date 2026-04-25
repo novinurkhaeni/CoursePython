74668da90dfc48ff24379d49a1d84272b3c62b9c
novinurkhaeni <novinurkhaeni.1@gmail.com>
2026-04-25 10:09:15 +0700

    manipulasi string

---
+++ python_dasar/manipulasi_string/manipulasi_stirng.py
@@ -0,0 +1,18 @@
+#MENGGABUNGKAN STRING
+#  ● Untuk menggabungkan string, kita bisa menggunakan tanda + (tambah)
+# ● Dan tipe data string tidak bisa digabungkan dengan tipe data lain seperti integer,
+# float atau boolean, kita harus konversi dulu tipe data lain menjadi tipe data string
+
+nama = "Budi"
+umur = 25
+
+# cara yang salah (akan error)
+# jika menggabungkan string dengan tioe data lain
+# pesan = "Nama saya " + nama + ", umur " + umur
+
+# cara yang benar
+pesan = "Nama saya " + nama + ", umur " + str(umur)
+print(pesan)
+
+# PANJANG STRING
+# Gunakan len() untuk mengetahui panjang string
