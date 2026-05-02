afea073c19398a372b6c401436206db762c43738
novinurkhaeni <novinurkhaeni.1@gmail.com>
2026-05-02 11:55:02 +0700

    operator aritmatika

---
+++ python_dasar/operator/operator_aritmatika.py
@@ -0,0 +1,17 @@
+# ● Operator aritmatika digunakan untuk melakukan operasi matematika.
+# ● Terdapat beberapa jenis operator aritmatika
+# ● Operator dasar, seperti + (tambah), - (kurang), * (kali), / (bagi)
+# ● Operator pembagian bulat dan modulo, seperti // (pembagian bulat), % (modulo, 
+# sisa pembagian)
+# ● Operator pangkat, seperti ** (pangkat)
+
+a = 10
+b = 3
+
+penjumlahan = a + b # 10 + 3 = 13
+pengurangan = a - b # 10 - 3 = 7
+perkalian = a * b # 10 * 3 = 30
+pembagian = a / b # 10 / 3 = 3.3333
+pembulatan = a // b # 10 / 3 = 3 (hasil tanpa desimal)
+sisa_bagi = a % b #sisa bagi dari 10 / 3 adalah 1
+pangkat = a ** b #10 pangkat 3 = 1000
